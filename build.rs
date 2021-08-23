8b3591088cdae536b028c7ae0687d725f0b62fab
Pooya Eimandar <pooya.eimandar@live.com>
2021-08-23 12:32:52 +0430

    v.0.1.3 working based on libc

--- build.rs
+++ build.rs
@@ -1,24 +1,20 @@
 fn main() {
-    //get profile
-    let profile = std::env::var("PROFILE").unwrap();
-
-    // set cxx build
-    let mut build = cxx_build::bridge("src/lib.rs");
+    let mut build = cc::Build::new();
 
     //set debug or release preprocessor
+    let profile = std::env::var("PROFILE").unwrap();
     if profile == "debug" {
-        build.define("DEBUG", "DEBUG");
-    } else {
-        build.define("NDEBUG", "NDEBUG");
+        build.define("CR_DEBUG", "CR_DEBUG");
     }
 
     build
-        .file("src/cxx/cr.cpp")
+        .cpp(true)
+        .file("src/cr.cpp")
         .flag_if_supported("-std=c++17")
-        .compile("cr-rs");
+        .compile("libcr-rs.a");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
-    println!("cargo:rerun-if-changed=src/cxx/cr/cr.h");
-    println!("cargo:rerun-if-changed=src/cxx/cr.hpp");
-    println!("cargo:rerun-if-changed=src/cxx/cr.cpp");
+    println!("cargo:rerun-if-changed=src/cr.h");
+    println!("cargo:rerun-if-changed=src/cr.cpp");
+    println!("cargo:rerun-if-changed=src/cr/cr.h");
 }
